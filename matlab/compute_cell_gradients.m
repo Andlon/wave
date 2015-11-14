bf9a79a5dbb7ecb7b6865921b5c5105d2623041e
Andreas Longva <andrebl@stud.ntnu.no>
2015-11-14 12:38:36 +0100

    Add compute_cell_gradients

--- matlab/compute_cell_gradients.m
+++ matlab/compute_cell_gradients.m
@@ -1,3 +1,11 @@
+function gradients = compute_cell_gradients( G, v, cells)
+gradients = zeros(numel(cells), 2);
+for c = 1:numel(cells)
+    cell = cells(c);
+    gradients(cell, :) = compute_cell_gradient(G, v, cell);
+end
+end
+
 function grad = compute_cell_gradient( G, v, cell )
 dimension = G.griddim;
 half_faces = G.cells.facePos(cell) : G.cells.facePos(cell + 1) - 1;
@@ -17,7 +25,7 @@ A = repmat(areas, 1, dimension);
 half_face_normals = D .* weighted_face_normals ./ A;
 
 % TODO: Determine if the following assumption is correct:
-% The normals of the half-faces are defined such that they face 
+% The normals of the half-faces are defined such that they face
 % from the faces into the interior of their respective cells.
 % In this case, we must flip the signs of our normals:
 half_face_normals = - half_face_normals;
