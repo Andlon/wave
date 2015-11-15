5f8d24eb63c8d0dbfa02ee2db5b633072b8f14f3
Espen Johansen Velsvik <espenvelsvik@gmail.com>
2015-11-15 19:21:28 +0100

    Add interpolation method and plot

--- matlab/charmeth.m
+++ matlab/charmeth.m
@@ -1,9 +1,11 @@
 % Solver based on characteristic curves
 % Solves the system Vt + l1*Vx = 0
 %                   Wt + l2*Wx = 0
+close all
+clear all
 
-T_max = 100;
-X_max = 100;
+T_max = 0.1;
+X_max = 10;
 N_space = 100;
 N_time = 100;
 delt = T_max/N_time;
@@ -13,8 +15,6 @@ eta = zeros(N_space,N_time);
 
 c1 = @(x,t) u(x,t) + sqrt(eta(x,t));
 c2 = @(x,t) u(x,t) - sqrt(eta(x,t));
-%V =  @(x,t) u(x,t) + 2*sqrt(eta(x,t));
-%W = @(x,t) u(x,t) - 2*sqrt(eta(x,t));
 u0 = @(x) 1;
 eta0 = @(x) sin(x)+1;
 
@@ -29,36 +29,58 @@ eta(:,1) = eta0(linspace(0,X_max,N_space));
 V_leftboundary = @(t) 1;
 W_rightboundary = @(t) 1;
 
+V = zeros(N_space,1);
+W = zeros(N_space,1);
 
-
-V = u0(x_V) + 2*sqrt(eta0(x_V));
-W = u0(x_W) - 2*sqrt(eta0(x_W));
+for k = 1:N_space
+    V(k) = u0(x_V(k)) + 2*sqrt(eta0(x_V(k)));
+    W(k) = u0(x_W(k)) - 2*sqrt(eta0(x_W(k)));
+end
 
 V(1) = V_leftboundary(0);
 W(end) = W_rightboundary(0);
 
+
 for i = 2:N_time
-    lambda1 = c1(x_V,t(i-1));
-    lambda2 = c2(x_W,t(i-1));
+
+    lambda1 = u(1:N_space,i-1) + sqrt(eta(1:N_space,i-1));
+    lambda2 = u(1:N_space,i-1) - sqrt(eta(1:N_space,i-1));
     
-    x_V = x_V + lambda1*delt;
-    x_W = x_W + lambda2*delt;
+    x_V = x_V + lambda1'*delt;
+    x_W = x_W + lambda2'*delt;
     
     % TO DO: Sort V according to x-values
     
     [x_V, x_Vindices] = sort(x_V);
-    Vted_sor = V(x_Vindices);
+    V_sorted = V(x_Vindices);
     
-    [x_W(i,:), x_Windices] = sort(x_W(i,:));
-    W_sorted = V(x_Windices);
-    
-    
+    [x_W, x_Windices] = sort(x_W);
+    W_sorted = W(x_Windices);
     
     
+    V = interp1(x_V,V_sorted,xq);
+    W = interp1(x_W,W_sorted,xq);
     
+    V(1) = V_leftboundary(t(i));
+    W(end) = W_rightboundary(t(i));
     
-    % Interpolate V and W values to find u and eta
-    % 
+    x_V = xq;
+    x_W = xq;
     
+    u(:,i) = (V+W)/2;
+    eta(:,i) = (V-W).^2/16;
+  
 end
 
+figure
+mesh(t,xq,u)
+title('Velocity')
+xlabel('x')
+ylabel('t')
+
+
+figure
+mesh(t,xq,eta)
+title('Surface')
+xlabel('x')
+ylabel('t')
