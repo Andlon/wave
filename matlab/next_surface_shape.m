e739e16296668695959c741acd1b7f51ca158b13
Andreas Longva <andrebl@stud.ntnu.no>
2015-11-16 17:05:19 +0100

    Fix incorrect calculation in next_surface_shape

--- matlab/next_surface_shape.m
+++ matlab/next_surface_shape.m
@@ -4,9 +4,7 @@ function eta = next_surface_shape( dx, dt, eta, grad_phi )
 % backward difference for the last element.
 % TODO: Better ways to approximate this?
 eta_x = [ diff(eta); eta(end) - eta(end-1) ] / dx;
-A = [ eta_x'; - ones(1, numel(eta_x)) ];
-dots = trace(grad_phi * A);
-eta = eta - dt * dots;
+eta = eta - dt * (grad_phi(:, 1) .* eta_x - grad_phi(:, 2));
 
 end
 
