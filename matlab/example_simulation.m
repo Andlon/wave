f4d157f28a0a9336c9da9fdc047345cabd767eb8
Andreas Longva <andrebl@stud.ntnu.no>
2015-11-22 21:54:56 +0100

    First implementation of scaling for example_simulation

--- matlab/example_simulation.m
+++ matlab/example_simulation.m
@@ -4,34 +4,88 @@ clear;
 close all;
 
 %% Configuration
-nx = 50;
-nz = 50;
-nt = 100;
-dt = 0.005;
+% Length of domain in meters
+L = 400;
+
+% Depth (vertical dimension) of domain in meters
+D = 100;
+
+% Number of cells in x, z and t dimensions
+nx = 100;
+nz = 100;
+nt = 10;
+
+% Size of time step in seconds
+dt = 0.01;
+
+% Gravitational acceleration in m/s^2
 g = 9.81;
 
-%% Boundary and initial conditions
-eta0 = @(x) zeros(size(x)); %  0.1 * sin(5 * pi * x);
-h =   @(x) zeros(size(x));  % 0.05 * cos(3 * pi * x);
+% The wave amplitude in meters (rough approximation)
+A = 10;
 
-X = linspace(0, 1, nx);
-phi_top = exp(-(X-0.5).^2) - 0.8;
-%phi_top = X;
+% Mass of foreign object hitting water in kg
+m = 1.404 * 10^11;
+
+% Velocity of foreign object hitting water in m/s
+v0 = 45;
+
+% Kinetic energy of foreign object as it hits the water, in J
+E_k = 0.5 * m * v0^2;
+
+% Dimension of impact zone, in meters (should be small compared to L)
+d = 5;
+
+%% Boundary and initial conditions
+%eta0 = @(x) 0.01 * sin(5 * pi * x);
+eta0 = @(x) zeros(size(x));
+h =   @(x) zeros(size(x));  % 0.05 * cos(3 * pi * x);
+%h = @(x) (x > 0.5) .* (1.5 * x - 0.75);
+
+X = L * linspace(0, 1, nx);
+phi_amplitude = E_k / L .* (1 / erf(L / (sqrt(2) * d)));
+phi_top = - phi_amplitude * exp(-(X - (L / 2)).^2/(d^2 / 2));
+%phi_top = zeros(size(X));
+
+%% Scales
+% Scales for time and potential
+T = L / sqrt(g * D);
+P = A * L * sqrt(g / D);
+
+% Set up scaled versions of variables
+h_scaled = @(x) h(x) / D;
+eta0_scaled = @(x) eta0(x) / A;
+dt_scaled = dt / T;
+phi_top_scaled = phi_top / P;
 
 %% Setup simulation
-sim = simulation(nx, nz, g, eta0, h);
+sim = simulation(nx, nz, g, eta0_scaled, h_scaled);
 
 %% Compute surface eta
-eta = solve_wave(sim, phi_top, dt, nt);
+eta = solve_wave(sim, phi_top_scaled, dt_scaled, nt, 'ShowProgress');
 
-x = linspace(0, 1, nx + 1);
+%% Inverse scale
+% Recover variables in original scale
+eta = A * eta;
+
+x = L * linspace(0, 1, nx + 1);
 t = dt * (0 : nt);
 
 %% Visualize
+eta_max = max(max(eta));
+eta_min = min(min(eta));
+eta_dist = abs(eta_max - eta_min);
 figure
-for i = 1:length(t)
-    newplot;
-    plot(x, eta(:, i));
-    ylim([-0.005, 0.005])
-    pause(0.1)
+fprintf('Press enter to start plotting...\n');
+while true
+    pause;
+    for i = 1:length(t)
+        newplot;
+        plot(x, eta(:, i));
+        %         ylim([-D, A]);
+        ylim([eta_min - eta_dist, eta_max + eta_dist]);
+        pauselength = 50 / length(t);
+        pause(min(pauselength, 0.2));
+    end
+    fprintf('Done plotting. Press enter to plot again or ctrl+c to abort. \n');
 end
