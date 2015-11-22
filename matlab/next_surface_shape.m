9fb14d277dd5fda84f5db912b665ef1a7673eda5
Andreas Longva <andrebl@stud.ntnu.no>
2015-11-22 15:09:00 +0100

    Use central differences in next_surface_shape

--- matlab/next_surface_shape.m
+++ matlab/next_surface_shape.m
@@ -3,8 +3,15 @@ function eta = next_surface_shape( dx, dt, eta, grad_phi )
 % Differentiate eta. For now simply we use forward differences, and
 % backward difference for the last element.
 % TODO: Better ways to approximate this?
-eta_x = [ diff(eta); eta(end) - eta(end-1) ] / dx;
+eta_x = differentiate(eta, dx);
 eta = eta - dt * (grad_phi(:, 1) .* eta_x - grad_phi(:, 2));
 
+end
+
+function Y = differentiate(X, dx)
+centraldiff = X(3:end) - X(1:end-2) / (2 * dx);
+boundary_forwarddiff = (X(2) - X(1)) / dx;
+boundary_backwarddiff = (X(end) - X(end - 1)) / dx;
+Y = [boundary_forwarddiff; centraldiff; boundary_backwarddiff];
 end
 
