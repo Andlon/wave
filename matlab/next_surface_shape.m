465327ee36dc64ba77c73271e55ded8fd259c5e7
Andreas Longva <andrebl@stud.ntnu.no>
2015-11-23 21:30:42 +0100

    Remove outdated comment

--- matlab/next_surface_shape.m
+++ matlab/next_surface_shape.m
@@ -1,8 +1,5 @@
 function eta = next_surface_shape( dx, dt, eta, grad_phi )
 
-% Differentiate eta. For now simply we use forward differences, and
-% backward difference for the last element.
-% TODO: Better ways to approximate this?
 eta_x = differentiate(eta, dx);
 eta = eta - dt * (grad_phi(:, 1) .* eta_x - grad_phi(:, 2));
 
